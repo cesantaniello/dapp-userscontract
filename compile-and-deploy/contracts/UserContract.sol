f9a22bdae46db2d5d85aa83e79f69ba39a9f9aa0
Carlos <carlos.e.santaniello@gmail.com>
2021-01-25 22:46:22 +0100

    3. functions join and getUser

--- compile-and-deploy/contracts/UserContract.sol
+++ compile-and-deploy/contracts/UserContract.sol
@@ -8,4 +8,15 @@ contract UsersContract {
     }
 
     mapping(address => User) private users;
+
+    function join(string name, string surName) public {
+        User storage user = users[msg.sender];
+        user.name = name;
+        user.surName = surName;
+    }
+
+    function getUser(address addr) public view returns (string, string){
+        User memory user = users[addr];
+        return (user.name, user.surName);
+    }
 }
