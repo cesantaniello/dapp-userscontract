a8c8fc91fe03ec2bef1a3ae13d5f05d7b349eb92
Carlos <carlos.e.santaniello@gmail.com>
2021-01-26 19:51:10 +0100

    5. Compile contract with solc

---
+++ compile-and-deploy/scripts/compile.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+const chalk = require('chalk');
+
+const contractPath = path.resolve(__dirname, "../contracts", "UsersContract.sol");
+const source = fs.readFileSync(contractPath, 'utf8');
+
+const { interface, bytecode } = solc.compile(source, 1).contracts[':UsersContract'];
+
+console.log(chalk.green(bytecode));
+console.log(chalk.cyan(interface));
